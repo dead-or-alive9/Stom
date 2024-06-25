4bd8a19870bf074f5b0e0e8e9057bc81d8a393a9
d <sako7212@gmail.com>
2024-06-25 10:44:09 +0500

    Добавил защиту

--- my_project/stom/views.py
+++ my_project/stom/views.py
@@ -1,14 +1,31 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_protect
 
 
 def aqn(request):
     return render(request, 'stom/AQN.html')
 
 
-def login(request):
-    return render(request, 'stom/login.html')
-
-
+@login_required
 def administration(request):
     return render(request, 'stom/administration.html')
+
+
+@csrf_protect
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('administration')
+        else:
+            return render(request, 'stom/login.html', {'error': 'Неправильный логин или пароль'})
+
+    return render(request, 'stom/login.html')

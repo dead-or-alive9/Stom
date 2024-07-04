e65828a6aa3ac3dc5691fd7a7dac4a74021aa732
aaaazamat <azamatsaribaev0005@email.com>
2024-07-04 10:37:51 +0500

    Mb g'a mag'liwmat qosildi

--- my_project/stom/urls.py
+++ my_project/stom/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', aqn, name='home'),
-    path('login/', login, name='login'),
-    path('administration/', administration, name='administration'),
-    path('doctors/', doctors, name='doctors'),
-]
+                  path('', aqn, name='home'),
+                  path('login/', login_view, name='login'),
+                  path('doctors/<slug:slug>/', doctor_detail, name='doctor_detail'),
+                  path('administration/', administration, name='administration'),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
